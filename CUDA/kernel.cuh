11df329a61cae460026b43cc40b67ddc3f1e7ed4
bokutotu <mushin.hudoushin@gmail.com>
2022-12-17 18:00:31 +0900

    cuda

--- CUDA/kernel.cuh
+++
@@ -1,4 +0,0 @@
-void alloc_gpu(float *dst, int size);
-void cpy_cpu2gpu(float *src, float *dst, int size);
-void cpy_gpu2cpu(float *src, float *dst, int size);
-__device__ void sum(float *a, float *b, float *c, int size);
